f8a99f03223f7255075683b81fe666cf5ba5d5e4
ejjang2030 <ejjang2030@gmail.com>
2024-05-14 23:46:46 +0900

    add : 로그인 화면 구현 및 파이어베이스 연결

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -7,52 +7,73 @@ import PostEdit from "pages/posts/edit";
 import ProfilePage from "pages/profile";
 import LoginPage from "pages/login";
 import SignupPage from "pages/signup";
+import {useState} from "react";
 
 export default function Router() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <>
       <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-        />
-        <Route
-          path='/posts'
-          element={<PostList />}
-        />
-        <Route
-          path='/posts/:id'
-          element={<PostDetail />}
-        />
-        <Route
-          path='/posts/new'
-          element={<PostNew />}
-        />
-        <Route
-          path='/posts/edit/:id'
-          element={<PostEdit />}
-        />
-        <Route
-          path='/profile'
-          element={<ProfilePage />}
-        />
-        <Route
-          path='/login'
-          element={<LoginPage />}
-        />
-        <Route
-          path='/signup'
-          element={<SignupPage />}
-        />
-        <Route
-          path='*'
-          element={
-            <Navigate
-              replace
-              to='/'
+        {isAuthenticated ? (
+          <>
+            <Route
+              path='/'
+              element={<Home />}
             />
-          }
-        />
+            <Route
+              path='/posts'
+              element={<PostList />}
+            />
+            <Route
+              path='/posts/:id'
+              element={<PostDetail />}
+            />
+            <Route
+              path='/posts/new'
+              element={<PostNew />}
+            />
+            <Route
+              path='/posts/edit/:id'
+              element={<PostEdit />}
+            />
+            <Route
+              path='/profile'
+              element={<ProfilePage />}
+            />
+            <Route
+              path='/login'
+              element={<LoginPage />}
+            />
+            <Route
+              path='/signup'
+              element={<SignupPage />}
+            />
+            <Route
+              path='*'
+              element={
+                <Navigate
+                  replace
+                  to='/'
+                />
+              }
+            />
+          </>
+        ) : (
+          <>
+            <Route
+              path='/login'
+              element={<LoginPage />}
+            />
+            <Route
+              path='/signup'
+              element={<SignupPage />}
+            />
+            <Route
+              path='*'
+              element={<LoginPage />}
+            />
+          </>
+        )}
       </Routes>
     </>
   );
